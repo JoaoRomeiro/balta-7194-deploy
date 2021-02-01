16af75a32c12391c7bab46a6d0c3d14d93eabec5
Joao Romeiro <j.romeiro@live.com>
2021-02-01 17:57:52 -0300

    Ativa a conexao com o SQL Server

--- Startup.cs
+++ Startup.cs
@@ -52,8 +52,8 @@ namespace Shop
                 };
             });
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-            //services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
 
             services.AddSwaggerGen(c => c.SwaggerDoc("v1",
             new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Shop", Version = "v1" }));
