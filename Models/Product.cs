48fc79c07b0fbc8e3883742daf15366ee5465ae1
Joao Romeiro <j.romeiro@live.com>
2021-02-02 17:28:23 -0300

    Impede o cadastro de colunas apenas com espaços vazios

--- Models/Product.cs
+++ Models/Product.cs
@@ -10,7 +10,7 @@ namespace Shop.Models
         [Column("Id")]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo é obrigatório")]
         [MaxLength(60, ErrorMessage = "Este campo deve conter no máximo 60 caracteres")]
         [MinLength(3, ErrorMessage = "Este campo deve conter no mínimo 3 caracteres")]
         [DataType("nvarchar")]
@@ -19,12 +19,12 @@ namespace Shop.Models
         [MaxLength(1024, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres")]
         public string Description { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo é obrigatório")]
         [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior do que zero")]
         public decimal Price { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
-        [Range(1, int.MaxValue, ErrorMessage ="Categoria inválida")]
+        [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
         public int CategoryId { get; set; }
 
         public Category Category { get; set; }
