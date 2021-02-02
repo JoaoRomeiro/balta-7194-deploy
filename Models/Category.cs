1e08c3d92c783b1f00d4b6555ae82a8e34ab8c0e
Joao Romeiro <j.romeiro@live.com>
2021-02-02 17:38:29 -0300

    Impede o cadastro de colunas apenas com espaços vazios

--- Models/Category.cs
+++ Models/Category.cs
@@ -10,7 +10,7 @@ namespace Shop.Models
         [Column("Id")]
         public int Id { get; set; }
 
-        //[Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo é obrigatório")]
         [MaxLength(60, ErrorMessage = "Este campo deve conter no máximo 60 caracteres")]
         [MinLength(3, ErrorMessage = "Este campo deve conter no mínimo 3 caracteres")]
         [DataType("nvarchar")]
--- Models/User.cs
+++ Models/User.cs
@@ -10,19 +10,19 @@ namespace Shop.Models
         [Column("Id")]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo é obrigatório")]
         [MaxLength(20, ErrorMessage = "Este campo deve conter no máximo 60 caracteres")]
         [MinLength(3, ErrorMessage = "Este campo deve conter no mínimo 3 caracteres")]
         [DataType("nvarchar")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo é obrigatório")]
         [MaxLength(20, ErrorMessage = "Este campo deve conter no máximo 60 caracteres")]
         [MinLength(3, ErrorMessage = "Este campo deve conter no mínimo 3 caracteres")]
         [DataType("nvarchar")]
         public string Password { get; set; }
 
-        [MaxLength(20, ErrorMessage = "Este campo deve conter no máximo 20 caracteres")]
+        [MaxLength(AllowEmptyStrings = false, 20, ErrorMessage = "Este campo deve conter no máximo 20 caracteres")]
         [DataType("nvarchar")]
         public string Role { get; set; }
     }
